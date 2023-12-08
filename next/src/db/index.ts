48015561dc97f27ff8c2dc1d5754185e22cc8394
Ryan <betrix303@gmail.com>
2023-12-08 11:41:54 -0500

    use env import for environment vars

--- next/src/db/index.ts
+++ next/src/db/index.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import { env } from "@/env";
 
-const client = postgres(process.env.DB_CONNECTION_STRING!);
+const client = postgres(env.DB_CONNECTION_STRING!);
 export const db = drizzle(client);
 
 export * from "drizzle-orm";
