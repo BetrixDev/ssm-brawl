5bd3cf95238caa3e65f4ab9a9d0f217a5602065e
Ryan <betrix303@gmail.com>
2023-12-25 12:46:55 -0500

    Remove unused methods

--- plugins/ssmb/src/main/java/dev/betrix/supersmashmobsbrawl/extensions/Particle.kt
+++
@@ -1,7 +0,0 @@
-package dev.betrix.supersmashmobsbrawl.extensions
-
-import org.bukkit.Particle
-
-fun Particle.fromKey(key: String): Particle? {
-    return Particle.values().find { it.key.value() == key }
-}
