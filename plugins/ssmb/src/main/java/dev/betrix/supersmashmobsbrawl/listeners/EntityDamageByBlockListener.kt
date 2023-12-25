b97a1516846c5be81a83f5a09428f13a01b479f9
Ryan <betrix303@gmail.com>
2023-12-25 10:50:48 -0500

    Teleport player when stuck in block

--- plugins/ssmb/src/main/java/dev/betrix/supersmashmobsbrawl/listeners/EntityDamageByBlockListener.kt
+++ plugins/ssmb/src/main/java/dev/betrix/supersmashmobsbrawl/listeners/EntityDamageByBlockListener.kt
@@ -1,16 +1,23 @@
 package dev.betrix.supersmashmobsbrawl.listeners
 
+import dev.betrix.supersmashmobsbrawl.SuperSmashMobsBrawl
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByBlockEvent
 import org.bukkit.event.entity.EntityDamageEvent
 
 class EntityDamageByBlockListener : Listener {
+    private val plugin = SuperSmashMobsBrawl.instance
 
     @EventHandler
     fun onEntityDamageByBlock(event: EntityDamageByBlockEvent) {
         if (event.cause == EntityDamageEvent.DamageCause.FALL) {
             event.isCancelled = true
+        } else if (event.cause == EntityDamageEvent.DamageCause.SUFFOCATION && event.entity is Player) {
+            if (event.entity.world == plugin.hub.worldInstance) {
+                plugin.hub.teleportPlayer(event.entity as Player)
+            }
         }
     }
 }
