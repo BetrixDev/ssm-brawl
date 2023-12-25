a9f9d3684930fdfd25b3da7c5d2735e4099e8f03
Ryan <betrix303@gmail.com>
2023-12-25 10:51:01 -0500

    Fix overflow

--- plugins/ssmb/src/main/java/dev/betrix/supersmashmobsbrawl/listeners/PlayerTeleportListener.kt
+++ plugins/ssmb/src/main/java/dev/betrix/supersmashmobsbrawl/listeners/PlayerTeleportListener.kt
@@ -1,6 +1,5 @@
 package dev.betrix.supersmashmobsbrawl.listeners
 
-import dev.betrix.supersmashmobsbrawl.maps.SSMBMap
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -10,11 +9,5 @@ class PlayerTeleportListener : Listener {
     @EventHandler
     fun onPlayerTeleport(event: PlayerTeleportEvent) {
         Bukkit.getServer().logger.info("Teleporting player between worlds")
-
-        if (event.from.world == event.to.world) {
-            return
-        }
-        
-        SSMBMap.mapFromWorld(event.to.world)?.teleportPlayer(event.player)
     }
 }
