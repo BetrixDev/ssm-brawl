48f4c724f6c5c706af640d2c6b4e64f19f51b988
Ryan <betrix303@gmail.com>
2024-03-02 18:16:51 -0500

    add plugin

---
+++ plugin/src/main/java/net/ssmb/SuperSmashMobsBrawl.kt
@@ -0,0 +1,13 @@
+package net.ssmb.ssmb
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SuperSmashMobsBrawl : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
