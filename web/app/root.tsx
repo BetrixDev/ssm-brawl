a78e00d28d653278c7b2fddc9766eb06999e4074
Ryan <betrix303@gmail.com>
2024-03-09 23:02:10 -0500

    web: add tailwind

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -7,6 +7,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import "./styles.css";
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
