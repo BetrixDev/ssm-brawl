a1fe872fa8fd7bd4acd2a5310c03f170980d55df
Ryan <betrix303@gmail.com>
2024-03-13 23:13:12 -0400

    plugin: add hunger passive

---
+++ plugin/src/main/java/net/ssmb/passives/HungerPassive.kt
@@ -0,0 +1,69 @@
+package net.ssmb.passives
+
+import com.github.shynixn.mccoroutine.bukkit.launch
+import com.github.shynixn.mccoroutine.bukkit.ticks
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import net.ssmb.SSMB
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.HandlerList
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityDamageEvent
+import kotlin.math.max
+import kotlin.math.min
+
+class HungerPassive(private val player: Player, meta: Map<String, String>) : IPassive, Listener {
+    private val plugin = SSMB.instance
+    private val secondsToDrain = meta["seconds_to_drain"]!!.toDouble()
+    private val hungerRestoreDelay = meta["hunger_restore_delay"]!!.toInt()
+
+    private var hungerJob: Job? = null
+    private var hungerTicks: Long = 0
+    private var lastHungerRestore = System.currentTimeMillis()
+
+    override fun createPassive() {
+        plugin.server.pluginManager.registerEvents(this, plugin)
+
+        hungerJob = plugin.launch {
+            while (true) {
+                hungerTicks++
+
+                player.saturation = 3f
+                player.exhaustion = 0f
+
+                if (player.foodLevel <= 0) {
+                    val damageEvent = EntityDamageEvent(player, EntityDamageEvent.DamageCause.STARVATION, 1.0)
+                    damageEvent.callEvent()
+                }
+
+                if ((hungerTicks % 10).toInt() == 0) {
+                    player.foodLevel = max(0, player.foodLevel - 1)
+                }
+
+                delay(20.ticks)
+            }
+        }
+    }
+
+    override fun destroyPassive() {
+        HandlerList.unregisterAll(this)
+
+        hungerJob?.cancel()
+    }
+
+    @EventHandler
+    fun onEntityDamageByEntity(event: EntityDamageByEntityEvent) {
+        if (event.damager != player) return
+
+        val currentTime = System.currentTimeMillis()
+
+        if (currentTime - lastHungerRestore < hungerRestoreDelay) return
+
+        lastHungerRestore = currentTime
+
+        val amount = max(1.0, event.damage / 2)
+        player.foodLevel = min(20.0, player.foodLevel + amount).toInt()
+    }
+}
