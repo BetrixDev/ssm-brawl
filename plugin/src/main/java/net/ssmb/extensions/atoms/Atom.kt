3abd11ee2a5ad4bf5dfb7dc2f6326e2ed3af18ca
Ryan <betrix303@gmail.com>
2024-03-20 22:28:26 -0400

    plugin: create Atom
    Fixes #50
    
    don't need a NullableAtom since you can just make the generic nullable and its good enough

---
+++ plugin/src/main/java/net/ssmb/extensions/atoms/Atom.kt
@@ -0,0 +1,29 @@
+package net.ssmb.extensions.atoms
+
+class Atom<T>(initialState: T) {
+    private var state: T = initialState
+    private val subscribers = mutableListOf<(T, T) -> Unit>()
+
+    fun get(): T {
+        if (state == null) throw IllegalStateException("Atom has not yet been initialized")
+
+        return state
+    }
+
+    fun set(newState: T) {
+        val oldValue = state
+        state = newState
+
+        subscribers.forEach {
+            it.invoke(newState, oldValue)
+        }
+    }
+
+    fun subscribe(callback: (newValue: T, oldValue: T) -> Unit): () -> Unit {
+        subscribers.add(callback)
+
+        return {
+            subscribers.remove(callback)
+        }
+    }
+}
