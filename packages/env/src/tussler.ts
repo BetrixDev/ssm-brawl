2550aa80aa0e23deed87633cadb46c58ab4a88ac
Ryan Bailey <rbailey@uwm.com>
2024-04-16 20:04:51 -0400

    env: make tussler replica vars optional

--- packages/env/src/tussler.ts
+++ packages/env/src/tussler.ts
@@ -3,8 +3,8 @@ import z from "zod";
 const envSchema = z.object({
   TUSSLER_TOKEN: z.string(),
   TUSSLER_URL: z.string(),
-  TUSSLER_SYNC_URL: z.string(),
-  TUSSLER_SYNC_INTERVAL: z.coerce.number()
+  TUSSLER_SYNC_URL: z.string().optional(),
+  TUSSLER_SYNC_INTERVAL: z.coerce.number().optional()
 });
 
 const env = envSchema.parse(process.env);
