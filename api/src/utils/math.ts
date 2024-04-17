4b26f9b91afcd4a98b34898ba5bb3c5d62e979c9
Ryan Bailey <rbailey@uwm.com>
2024-04-17 09:11:56 -0400

    format

--- api/src/utils/math.ts
+++ api/src/utils/math.ts
@@ -1,12 +1,11 @@
+import { customAlphabet } from "nanoid";
 
-import {customAlphabet} from "nanoid"
-
-const numberNanoid = customAlphabet("1234567890")
+const numberNanoid = customAlphabet("1234567890");
 
 export function useRandomInt(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export function useRandomId(length: number) {
-  return numberNanoid(length)
+  return numberNanoid(length);
 }
