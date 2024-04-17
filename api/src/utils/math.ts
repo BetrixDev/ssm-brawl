4a9d1ad50fae1562f199d789b2d2e25e28787c02
Ryan Bailey <rbailey@uwm.com>
2024-04-17 09:10:23 -0400

    api: use nanoids for custom ids

--- api/src/utils/math.ts
+++ api/src/utils/math.ts
@@ -1,15 +1,12 @@
-import { generateRandomInteger } from "oslo/crypto";
+
+import {customAlphabet} from "nanoid"
+
+const numberNanoid = customAlphabet("1234567890")
 
 export function useRandomInt(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export function useRandomId(length: number) {
-  let out = "";
-
-  for (let i = 0; i < length; i++) {
-    out += generateRandomInteger(10);
-  }
-
-  return out;
+  return numberNanoid(length)
 }
