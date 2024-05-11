dcf312a82fc8deada76c5e7a6e3cfe957d3ae866
Ryan Bailey <rbailey@uwm.com>
2024-05-11 09:38:16 -0400

    fix env

--- api/vite.config.ts
+++ api/vite.config.ts
@@ -6,5 +6,17 @@ export default defineConfig({
       extension: [".ts"],
     },
     globalSetup: ["./setup-tests.ts"],
+    env: {
+      NODE_ENV: "test",
+      JWT_PRIVATE_KEY: "private_key",
+      API_HOST: "localhost",
+      API_PORT: "8080",
+      API_PROTOCOL: "http",
+      API_TOKEN_SECRET: "token_secret",
+      TUSSLER_URL: "this_doesnt_matter",
+      TUSSLER_TOKEN: "fake token",
+      WRANGLER_HOST: "localhost",
+      WRANGLER_PORT: "27017",
+    },
   },
 });
