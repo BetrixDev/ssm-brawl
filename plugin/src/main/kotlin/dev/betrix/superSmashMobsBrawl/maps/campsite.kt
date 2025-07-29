3f4aeb6389b22cc580077d50987d1694779e9294
Ryan <betrix303@gmail.com>
2025-07-28 22:35:31 -0400

    add more spawnpoints to campsite map

--- plugin/src/main/kotlin/dev/betrix/superSmashMobsBrawl/maps/campsite.kt
+++ plugin/src/main/kotlin/dev/betrix/superSmashMobsBrawl/maps/campsite.kt
@@ -11,5 +11,10 @@ val campsiteMap = defineMap {
 
     addCreator("PLACEHOLDER_UUID")
 
-    spawnPoints { at(13.0, 106.0, 33.0) }
+    spawnPoints {
+        at(13.0, 106.0, 33.0)
+        at(18.0, 104.0, 62.0)
+        at(-31.0, 106.0, 59.0)
+        at(-15.0, 107.0, 27.0)
+    }
 }
