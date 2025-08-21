4bff45a574511da6381fe8a236c1d596517cb0e3
Ryan <betrix303@gmail.com>
2025-08-20 22:40:49 -0400

    new event for queue pop

---
+++ plugin/src/main/kotlin/dev/betrix/superSmashMobsBrawl/events/QueuePopEvent.kt
@@ -0,0 +1,6 @@
+package dev.betrix.superSmashMobsBrawl.events
+
+import gg.flyte.twilight.event.TwilightEvent
+import org.bukkit.entity.Player
+
+class QueuePopEvent(minigameId: String, players: List<Player>) : TwilightEvent()
--- plugin/src/main/kotlin/dev/betrix/superSmashMobsBrawl/services/QueueService.kt
+++ plugin/src/main/kotlin/dev/betrix/superSmashMobsBrawl/services/QueueService.kt
@@ -5,6 +5,7 @@ import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.Result
 import dev.betrix.superSmashMobsBrawl.Manageable
 import dev.betrix.superSmashMobsBrawl.SuperSmashMobsBrawl
+import dev.betrix.superSmashMobsBrawl.events.QueuePopEvent
 import dev.betrix.superSmashMobsBrawl.minigames.PrototypingMinigame
 import dev.betrix.superSmashMobsBrawl.minigames.TeamBasedStocksMinigame
 import dev.betrix.superSmashMobsBrawl.models.MinigameTeam
@@ -115,7 +116,7 @@ object QueueService : Manageable(), KoinComponent {
         val requiredPlayers = getRequiredPlayersForMinigame(minigameDef)
 
         if (requiredPlayers <= 0) {
-            logger.severe(
+            logger.warning(
                 "Minigame ${minigameDef.id} has invalid player requirement: $requiredPlayers"
             )
             return
@@ -133,68 +134,10 @@ object QueueService : Manageable(), KoinComponent {
             // Remove chosen entries from the master queue
             playersToStart.forEach { queue.remove(it) }
 
-            onMinigameCanStart(minigameDef, playersToStart)
+            QueuePopEvent(minigameDef.id, playersToStart.map { it.player })
 
             // Drop the used players from the local list and continue if we can start more
             available = available.drop(requiredPlayers).toMutableList()
         }
-    }
-
-    private fun onMinigameCanStart(minigameDef: MinigameDef, queuedPlayers: List<QueueEntry>) {
-        val entriesToUse: List<QueueEntry> =
-            when (minigameDef) {
-                is TeamBasedStocksMinigameDef -> {
-                    val playersPerTeam = minigameDef.playersPerTeam
-                    val amountOfTeams = minigameDef.amountOfTeams
-
-                    // Take only the required number of players for all teams
-                    val totalPlayersNeeded = playersPerTeam * amountOfTeams
-                    queuedPlayers.take(totalPlayersNeeded)
-                }
-
-                is FfaMinigameDef -> {
-                    queuedPlayers.take(minigameDef.maxPlayers)
-                }
-            }
-
-        val gameId = UUID.randomUUID().toString()
-
-        when (minigameDef) {
-            is TeamBasedStocksMinigameDef -> {
-                val playersPerTeam = minigameDef.playersPerTeam
-                val amountOfTeams = minigameDef.amountOfTeams
-
-                val players: List<Player> = entriesToUse.map { it.player }
-
-                val teams: List<MinigameTeam> =
-                    (0 until amountOfTeams).map { teamIndex ->
-                        val startIndex = teamIndex * playersPerTeam
-                        val endIndex = startIndex + playersPerTeam
-                        val teamPlayers = players.subList(startIndex, endIndex).toMutableList()
-                        // Initial stocks value will be set during minigame init from definition
-                        MinigameTeam(teamPlayers, minigameDef.stocks)
-                    }
-
-                val minigame = TeamBasedStocksMinigame(minigameDef.id, gameId, teams)
-                minigameService.handleMinigameSetup(minigame)
-            }
-
-            is FfaMinigameDef -> {
-                val players: List<Player> = entriesToUse.map { it.player }
-
-                when (minigameDef.id) {
-                    "prototyping" -> {
-                        val minigame = PrototypingMinigame(minigameDef.id, gameId, players)
-                        minigameService.handleMinigameSetup(minigame)
-                    }
-
-                    else -> {
-                        // No-op for unknown ids for now
-                    }
-                }
-            }
-        }
-
-        // TODO: Figure out new minigame flow
     }
 }
