6550a91c1347695e8e8c85eb25e0100276653e00
Ryan <betrix303@gmail.com>
2025-09-16 18:29:36 -0400

    vercel target

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tailwindcss(),
-    tanstackStart({ customViteReactPlugin: true }),
+    tanstackStart({ customViteReactPlugin: true, target: "vercel" }),
     viteReact(),
   ],
 });
