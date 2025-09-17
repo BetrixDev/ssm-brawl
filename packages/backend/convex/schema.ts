f26cdb73b2be06d323c746b4bfb5df3128318468
Ryan <betrix303@gmail.com>
2025-09-16 20:33:04 -0400

    add schema for kv table

--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -1,3 +1,9 @@
-import { defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
 
-export default defineSchema({});
+export default defineSchema({
+  kv: defineTable({
+    key: v.string(),
+    value: v.any(),
+  }).index("by_key", ["key"]),
+});
