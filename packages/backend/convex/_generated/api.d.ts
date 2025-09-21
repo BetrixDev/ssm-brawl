1c455dde0ddbdcb354dcb1304873f0f92287622a
Ryan <betrix303@gmail.com>
2025-09-20 21:12:41 -0400

    up

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -8,7 +8,11 @@
  * @module
  */
 
-import type { ApiFromModules, FilterApi, FunctionReference } from "convex/server";
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
 import type * as common from "../common.js";
 import type * as healthCheck from "../healthCheck.js";
 import type * as http from "../http.js";
@@ -32,5 +36,11 @@ declare const fullApi: ApiFromModules<{
   players: typeof players;
   schemas: typeof schemas;
 }>;
-export declare const api: FilterApi<typeof fullApi, FunctionReference<any, "public">>;
-export declare const internal: FilterApi<typeof fullApi, FunctionReference<any, "internal">>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
