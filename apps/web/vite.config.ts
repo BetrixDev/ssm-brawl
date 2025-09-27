2906b1b690d6391cf9e2ed644153855adb196f7d
Ryan <betrix303@gmail.com>
2025-09-27 16:43:18 -0400

    some changes

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro-nightly/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), nitro(), , viteReact()],
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
+  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
 });
