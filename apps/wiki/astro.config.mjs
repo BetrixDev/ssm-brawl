6608032b84a73c49d885e391f9315c85e0ed5538
Ryan <betrix303@gmail.com>
2025-10-02 22:03:33 -0400

    astro starlight wiki

---
+++ apps/wiki/astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+import starlightThemeRapide from "starlight-theme-rapide";
+
+// https://astro.build/config
+export default defineConfig({
+  server: {
+    port: 1337,
+  },
+  integrations: [
+    starlight({
+      plugins: [starlightThemeRapide()],
+      title: "SSMBrawl Wiki",
+      social: [
+        { icon: "external", label: "Website", href: "https://ssmbrawl.com" },
+        { icon: "github", label: "GitHub", href: "https://github.com/BetrixDev/ssm-brawl" },
+        { icon: "discord", label: "Discord", href: "https://discord.gg/ssm-brawl" },
+      ],
+      sidebar: [
+        {
+          label: "Kits",
+          autogenerate: { directory: "kits" },
+        },
+      ],
+    }),
+  ],
+});
